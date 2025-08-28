158665aad6cb6b36a87ef2012550bb5873c565a7
gabiseggio <gabiseggio@gmail.com>
2025-08-28 11:36:02 -0300

    novo
    visual

--- adocao/config/utils.php
+++
@@ -1,8 +0,0 @@
-<?php
-function resposta($sucesso, $mensagem, $extra = []) {
-    echo json_encode(array_merge([
-        "sucesso" => $sucesso,
-        "mensagem" => $mensagem
-    ], $extra));
-    exit;
-}
